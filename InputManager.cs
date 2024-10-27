8674a5c989a7348b4aa39490bb0a53f7f0beb8de
Evan <evan.nilsson@students.rsd.edu>
2024-10-26 17:46:13 -0700

    Update InputManager.cs

--- InputManager.cs
+++ InputManager.cs
@@ -1,11 +1,22 @@
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 internal class InputManager {
-    private static KeyboardState keyboardState;
-    private static List<Keys> framePresses;
+    public static KeyboardState keyboardState {get; private set;}
+    public static MouseState mouseState {get; private set;}
+    private static List<Keys> framePressesKeyboard;
+    private static List<int> framePressesMouse;
     public static bool IsKeyDown(Keys key) {return keyboardState.IsKeyDown(key);}
     public static bool IsKeyUp(Keys key) {return keyboardState.IsKeyUp(key);}
-    public static bool IsKeyPress(Keys key) { if (!framePresses.Contains(key) && keyboardState.IsKeyDown(key)) { framePresses.Add(key); return true; } else { if (keyboardState.IsKeyUp(key) && framePresses.Contains(key)) { framePresses.Remove(key); } return false; }}
-    public static void Update() {if (framePresses == null) { framePresses = new(); } keyboardState = Keyboard.GetState(); }
+    public static bool IsKeyPress(Keys key) { if (!framePressesKeyboard.Contains(key) && keyboardState.IsKeyDown(key)) { framePressesKeyboard.Add(key); return true; } else { if (keyboardState.IsKeyUp(key) && framePressesKeyboard.Contains(key)) { framePressesKeyboard.Remove(key); } return false; }}
+    public static bool IsLeftPress() {int key = 1; if (!framePressesMouse.Contains(key) && mouseState.LeftButton == ButtonState.Pressed) { framePressesMouse.Add(key); return true; } else { if (mouseState.LeftButton == ButtonState.Released && framePressesMouse.Contains(key)) { framePressesMouse.Remove(key); } return false; }}
+    public static bool IsRightPress() {int key = 2; if (!framePressesMouse.Contains(key) && mouseState.RightButton == ButtonState.Pressed) { framePressesMouse.Add(key); return true; } else { if (mouseState.RightButton == ButtonState.Released && framePressesMouse.Contains(key)) { framePressesMouse.Remove(key); } return false; }}
+    public static bool IsMiddlePress() {int key = 3; if (!framePressesMouse.Contains(key) && mouseState.MiddleButton == ButtonState.Pressed) { framePressesMouse.Add(key); return true; } else { if (mouseState.MiddleButton == ButtonState.Released && framePressesMouse.Contains(key)) { framePressesMouse.Remove(key); } return false; }}
+    public static void Update() {if (framePressesKeyboard == null) { framePressesKeyboard = new(); }if (framePressesMouse == null) { framePressesMouse = new(); }   mouseState = Mouse.GetState(); keyboardState = Keyboard.GetState(); }
+    public static Vector2 GetMousePosition() { return mouseState.Position.ToVector2(); }
+    public static int GetMouseX() { return mouseState.Position.X; }
+    public static int GetMouseY() { return mouseState.Position.Y; }
+    public static int GetScrollWheel() { return mouseState.ScrollWheelValue; }
 }
